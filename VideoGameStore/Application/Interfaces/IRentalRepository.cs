f4fd7d063c0c8494372859ba0c780f8e16236a68
mmd-mshdy <mohammad1333609@gmail.com>
2026-01-03 21:51:04 +0330

    Adding IRentalRepository

---
+++ VideoGameStore/Application/Interfaces/IRentalRepository.cs
@@ -0,0 +1,11 @@
+﻿using VideoGameStore.Domain.Entities;
+namespace VideoGameStore.Application.Interfaces
+{
+    public interface IRentalRepository
+    {
+        Task<Rental?> GetByIdAsync(int id);
+        Task<IReadOnlyList<Rental>> GetActiveByCustomerIdAsync(int customerId);
+        Task<IReadOnlyList<Rental>> GetOverdueAsync(DateTime now);
+        Task Add(Rental rental);
+    }
+}
