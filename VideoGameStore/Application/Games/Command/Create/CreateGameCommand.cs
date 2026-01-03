a39ff45076d1b23ec4f0e9467e2f251b845b3b1c
mmd-mshdy <mohammad1333609@gmail.com>
2026-01-03 21:53:19 +0330

    Adding Final changes

--- VideoGameStore/Application/Games/Command/Create/CreateGameCommand.cs
+++ VideoGameStore/Application/Games/Command/Create/CreateGameCommand.cs
@@ -1,6 +1,7 @@
 ﻿using VideoGameStore.Application.Interfaces;
 using VideoGameStore.Domain.common;
 using VideoGameStore.Domain.Entities;
+
 namespace VideoGameStore.Application.Games.Command.Create
 {
     public record CreateGameCommand(string Name ,string Genre , decimal Price , DateTime ReleaseDate): ICommand<Result<Game>>;
--- VideoGameStore/Application/Games/Command/Create/CreateGameHandler.cs
+++ VideoGameStore/Application/Games/Command/Create/CreateGameHandler.cs
@@ -3,6 +3,7 @@ using VideoGameStore.Application.Interfaces;
 using VideoGameStore.Domain.common;
 using VideoGameStore.Domain.common.Errors;
 using VideoGameStore.Domain.Entities;
+using VideoGameStore.Domain.ValueObjects;
 
 namespace VideoGameStore.Application.Games.Command.Create
 {
@@ -20,7 +21,8 @@ namespace VideoGameStore.Application.Games.Command.Create
         {
             try
             {
-                var game = new Game(request.Name, request.Genre, request.Price, request.ReleaseDate);
+                var price = new Money(request.Price);
+                var game = new Game(request.Name, request.Genre, price, request.ReleaseDate);
                 await _gameRepository.AddAsync(game);
                 await _unitOfWork.CompleteAsync();
                 return Result.Success(game);
