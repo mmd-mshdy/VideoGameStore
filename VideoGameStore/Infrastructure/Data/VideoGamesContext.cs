200e56268b57141773528f66cf121ad16ada7744
Mahdifatehi <mfatehi8375@gmail.com>
2026-01-03 21:21:16 +0330

    Modifying Databse Context

--- VideoGameStore/Infrastructure/Data/VideoGamesContext.cs
+++ VideoGameStore/Infrastructure/Data/VideoGamesContext.cs
@@ -10,13 +10,8 @@ namespace VideoGameStore.Infrastructure.Data
         public DbSet<Game>? Games { get; set; } = null;
         public DbSet<Customer>? Customers { get; set; } = null; 
         public DbSet<Manager>? Managers { get; set; } = null; 
-        public DbSet<Transaction>? Transactions { get; set; } = null; 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Transaction>().Property(x => x.Amount).HasPrecision(18,2);
-            modelBuilder.Entity<Game>().Property(x => x.Price).HasPrecision(18,2);
-            modelBuilder.Entity<Customer>().Property(x => x.WalletBalance).HasPrecision(18,2);
-;        }
+        public DbSet<Transaction>? Transactions { get; set; } = null;
+        public DbSet<Rental>? Rentals { get; set; } = null;
 
         public async Task CompleteAsync()
         {
